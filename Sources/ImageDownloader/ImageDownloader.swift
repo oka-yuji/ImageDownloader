8e8084bd2f81ea144af8440fb3ac844e160347c7
岡優志 <yu.j.0513@gmail.com>
2021-11-06 22:01:01 +0900

    feat:add new project

---
+++ Sources/ImageDownloader/ImageDownloader.swift
@@ -0,0 +1,6 @@
+public struct ImageDownloader {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/ImageDownloaderTests/ImageDownloaderTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ImageDownloader
+
+final class ImageDownloaderTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ImageDownloader().text, "Hello, World!")
+    }
+}
